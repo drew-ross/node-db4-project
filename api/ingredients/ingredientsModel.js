821d1c403f491e6f235fcc561e80fb17acb9f561
Drew Ross <mail.drew.ross@gmail.com>
2020-08-13 15:34:15 -0700

    Create getIngredients and getIngredientRecipes db methods and GET endpoints.

--- api/ingredients/ingredientsModel.js
+++ api/ingredients/ingredientsModel.js
@@ -1,15 +1,22 @@
 const db = require('../../data/dbConfig');
 
-// module.exports = {
-//   add,
-//   find,
-//   findById,
-//   update,
-//   remove,
-// };
+module.exports = {
+  getIngredients,
+  getIngredientRecipes
+};
 
-// function add(ingredient) {
-//   return db('ingredients')
-//   .insert(ingredient)
-//   .then()
-// }
+function getIngredients() {
+  return db('ingredients')
+    .then(ingredients => ingredients)
+    .catch(err => console.log(err));
+}
+
+function getIngredientRecipes(id) {
+  return db('recipes as r')
+    .join('recipe_ingredients as ri', 'ri.recipe_id', 'r.id')
+    .join('ingredients as i', 'i.id', 'ri.ingredient_id')
+    .where('ri.ingredient_id', id)
+    .select('r.id as recipe_id', 'r.name as recipe_name', 'i.name')
+    .then(recipes => recipes)
+    .catch(err => console.log(err));
+}
--- api/ingredients/ingredientsRouter.js
+++ api/ingredients/ingredientsRouter.js
@@ -5,7 +5,16 @@ const ingDb = require('./ingredientsModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json({ message: 'endpoint up' });
+  ingDb.getIngredients()
+    .then(ingredients => res.status(200).json(ingredients))
+    .catch(error => res.status(500).json({ message: 'There was a problem with the server.', error }));
+});
+
+router.get('/:id/recipes', (req, res) => {
+  const { id } = req.params
+  ingDb.getIngredientRecipes(id)
+    .then(ingredients => res.status(200).json(ingredients))
+    .catch(error => res.status(500).json({ message: 'There was a problem with the server.', error }));
 });
 
 module.exports = router;
