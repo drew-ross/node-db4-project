8fd11df423eb64f25cb27fc47c8d7e2929dfefb5
Drew Ross <mail.drew.ross@gmail.com>
2020-08-13 13:33:43 -0700

    Write and run init migration to set up database.

--- data/migrations/20200813124201_init.js
+++ data/migrations/20200813124201_init.js
@@ -1,8 +1,43 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments('id');
+      tbl.text('name', 255)
+        .notNullable();
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments('id');
+      tbl.text('name', 255)
+        .notNullable();
+    })
+    .createTable('recipe_ingredients', tbl => {
+      tbl.increments('id');
+      tbl.integer('step')
+        .notNullable()
+        .unsigned();
+      tbl.float('quantity')
+        .notNullable()
+        .unsigned();
+      //foreign keys
+      tbl.integer('recipe_id')
+        .notNullable()
+        .unsigned()
+        .references('recipes.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.integer('ingredient_id')
+        .notNullable()
+        .unsigned()
+        .references('ingredients.id')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipe_ingredients');
 };
